8d41cd9b7e9d4af5b29d7a32330b14ee6a2dc53c
Edward Jibson <jibson@tuta.io>
2025-04-14 19:20:58 +0100

    Update Book.js

--- models/Book.js
+++ models/Book.js
@@ -8,4 +8,4 @@ const bookSchema = new mongoose.Schema({
     available: { type: Boolean, default: true }
 });
 
-module.exports = mongoose.model('Book', bookSchema);
+module.exports = mongoose.model('Lorem_ipsum_dolor_sit_amet,_consectetuer_adipiscing_elit._Aenean_commodo_ligula_e', bookSchema);
